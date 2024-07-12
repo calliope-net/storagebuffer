9dc7fe2b02a8f8ac2ac97054f20bb44312bf2184
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-12 20:47:47 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ storageBuffer.ts
@@ -0,0 +1,20 @@
+
+namespace storageBuffer{
+
+    //% block
+    export function putBuffer(buffer: Buffer): void {
+        // let managedValue = Math.floor(value * 100);
+        // storage.putValueInt(storagesInt[key], managedValue);
+        storage.putValueInt("i2", buffer.getNumber(NumberFormat.UInt32LE, 0))
+    }
+
+    //% block
+    export function getBuffer(): Buffer {
+        // let value = getValueInt(storagesInt[key]);
+        // return value / 100;
+        let buffer = Buffer.create(4)
+        buffer.setNumber(NumberFormat.UInt32LE, 0, storage.getValueInt("i2"))
+        return buffer
+    }
+
+}
