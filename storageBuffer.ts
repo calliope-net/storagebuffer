6ad05f695733880ec24e24f83be75fe4e0875cbf
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-17 11:10:09 +0200

    Aktualisiere pxt.json, main.blocks, storageBuffer.ts

--- storageBuffer.ts
+++ storageBuffer.ts
@@ -1,14 +1,16 @@
 
-namespace storage{
+namespace storage {
 
-    //% block
+    //% group="Buffer in Flash schreiben und lesen"
+    //% block="Buffer %buffer in Slot 2 speichern (UInt32LE)" weight=2
     export function putBuffer(buffer: Buffer): void {
         // let managedValue = Math.floor(value * 100);
         // storage.putValueInt(storagesInt[key], managedValue);
         storage.putValueInt("i2", buffer.getNumber(NumberFormat.UInt32LE, 0))
     }
 
-    //% block
+    //% group="Buffer in Flash schreiben und lesen"
+    //% block="Buffer aus Slot 2 lesen (UInt32LE)" weight=1
     export function getBuffer(): Buffer {
         // let value = getValueInt(storagesInt[key]);
         // return value / 100;
