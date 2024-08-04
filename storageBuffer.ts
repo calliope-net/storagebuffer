7701053cf493af11cf310e55f9eb1cd1e4b68830
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-04 16:17:18 +0200

    Aktualisiere storageBuffer.ts

--- storageBuffer.ts
+++ storageBuffer.ts
@@ -1,21 +1,24 @@
 
 namespace storage {
 
+    const key = "i2"
+    let i2 = storage.getValueInt(key)
+
     //% group="Buffer in Flash schreiben und lesen"
     //% block="Buffer %buffer in Slot 2 speichern (UInt32LE)" weight=2
     export function putBuffer(buffer: Buffer): void {
-        // let managedValue = Math.floor(value * 100);
-        // storage.putValueInt(storagesInt[key], managedValue);
-        storage.putValueInt("i2", buffer.getNumber(NumberFormat.UInt32LE, 0))
+        let value = buffer.getNumber(NumberFormat.UInt32LE, 0)
+        if (i2 != value) { // nur bei Änderung in Flash schreiben
+            i2 = value
+            storage.putValueInt(key, value)
+        }
     }
 
     //% group="Buffer in Flash schreiben und lesen"
     //% block="Buffer aus Slot 2 lesen (UInt32LE)" weight=1
     export function getBuffer(): Buffer {
-        // let value = getValueInt(storagesInt[key]);
-        // return value / 100;
         let buffer = Buffer.create(4)
-        buffer.setNumber(NumberFormat.UInt32LE, 0, storage.getValueInt("i2"))
+        buffer.setNumber(NumberFormat.UInt32LE, 0, storage.getValueInt(key))
         return buffer
     }
 
